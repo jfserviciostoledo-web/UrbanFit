5b7dc91f83d5b051d9634d006c8b813ab77f81f4
rubeng19 <rubengalo2006@gmail.com>
2026-03-23 11:49:05 +0100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,20 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// Configuración para poder leer datos de formularios (JSON)
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Servidor del Gimnasio UrbanFit funcionando 🚀');
+// RUTA DE REGISTRO
+app.post('/registro', (req, res) => {
+    const { nombre, email, password } = req.body;
+    
+    // De momento, simulamos que lo guardamos (luego lo conectaremos a MySQL)
+    console.log(`Nuevo registro: ${nombre} - ${email}`);
+    
+    res.json({ 
+        mensaje: `¡Usuario ${nombre} recibido con éxito en el servidor!` 
+    });
 });
 
 app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
+    console.log(`Servidor de UrbanFit corriendo en http://localhost:${port}`);
 });
