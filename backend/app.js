0b5f39a4308f9ceb6afa6edbe8fbaf6cc86b4ff0
rubeng19 <rubengalo2006@gmail.com>
2026-03-23 11:37:48 +0100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
+// Configuración para poder leer datos de formularios (JSON)
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Servidor del Gimnasio UrbanFit funcionando 🚀');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
