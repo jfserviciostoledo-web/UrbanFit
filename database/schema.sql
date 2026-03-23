2a0daf7bb5beebd86ffa7a1311bdca1abfa007fb
rubeng19 <rubengalo2006@gmail.com>
2026-03-23 11:36:28 +0100

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,1 +1,26 @@
+-- Tabla de Usuarios
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL, -- Aquí irá la contraseña encriptada
+    fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
+-- Tabla de Clases 
+CREATE TABLE clases (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    horario VARCHAR(50) NOT NULL,
+    capacidad_max INT DEFAULT 20
+);
+
+-- Tabla de Reservas
+CREATE TABLE reservas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario_id INT,
+    clase_id INT,
+    fecha_reserva TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
+    FOREIGN KEY (clase_id) REFERENCES clases(id)
+);
