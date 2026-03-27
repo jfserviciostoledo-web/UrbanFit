777eb5d1d3c057d55ebebe1728258c2d4b7b1fce
rubeng19 <rubengalo2006@gmail.com>
2026-03-27 13:49:07 +0100

    Update bookingRoutes.js

--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const bookingController = require('../controllers/bookingController');
-// Definimos los caminos 
+const authMiddleware = require('../middlewares/authMiddleware');
+
+// 1. RUTA PÚBLICA (Ver el horario)
 router.get('/clases', bookingController.obtenerClases);
-router.post('/reservar', bookingController.reservarClase);
-router.get('/mis-reservas/:usuarioId', bookingController.obtenerMisReservas);
-router.delete('/cancelar/:id', bookingController.cancelarReserva);
+
+// 2. RUTAS PROTEGIDAS (Solo con Token válido)
+// Ahora, para reservar o cancelar, el servidor exige el token)
+router.post('/reservar', authMiddleware, bookingController.reservarClase);
+router.get('/mis-reservas/:usuarioId', authMiddleware, bookingController.obtenerMisReservas);
+router.delete('/cancelar/:id', authMiddleware, bookingController.cancelarReserva);
 
 module.exports = router;
+
