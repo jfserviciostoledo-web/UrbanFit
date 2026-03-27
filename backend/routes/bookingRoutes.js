6583761ee30d51c9736a34e4bf4155d23803d09e
rubeng19 <rubengalo2006@gmail.com>
2026-03-27 12:53:14 +0100

    Update bookingRoutes.js

--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const bookingController = require('../controllers/bookingController');
-
-// Definimos los caminos limpios
+// Definimos los caminos 
+router.get('/clases', bookingController.obtenerClases);
 router.post('/reservar', bookingController.reservarClase);
+router.get('/mis-reservas/:usuarioId', bookingController.obtenerMisReservas);
 router.delete('/cancelar/:id', bookingController.cancelarReserva);
 
-module.exports = router
+module.exports = router;
